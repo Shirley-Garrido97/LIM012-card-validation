03496f3f51f4f4b8d44d31ad16fd8c95c6caa01e
Shirley-Garrido97 <garridonavarroshirley@gmail.com>
2020-02-14 12:20:58 -0500

    encontrando la luz

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,29 @@
-const validator = {
-  // ...
-};
+//declarando variable para llamar al boton de validar tarjeta.
+const validator = document.getElementById("btnValidarCard");
+// se le indica que cuando se haga click que es lo que debe realizar:
+validator.addEventListener( "click" , function () {
+  let numCard = document.getElementById('numCard').value;
+  if (numCard.length>12 && numCard.length<=16){
+      alert("Acceso Correcto");
+    } else if (numCard.length<13){
+      alert("Falta Digitos");
+    } else {
+      alert("Exceso de Digitos")
+    }
+    console.log(numCard);
+
+// declarando un array que contendrá los numeros ingresados de la tarjeta estean en orden inverso
+  let reverseNumber = numCard.split("").reverse();
+  console.log(reverseNumber);
+
+//extrayendo los numeros de las posiciones impares, que en realidad son posiciones pares
+  for (var i = 0; i < reverseNumber.length; i=i+1) {
+    if (i%2==1){
+      console.log(reverseNumber[i]*2); // multiplicando por 2
+    }
+  }
+
+
+} );
 
 export default validator;
