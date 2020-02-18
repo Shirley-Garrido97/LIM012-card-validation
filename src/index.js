5322c635ea8c8db374fc97a20b0bbe39523dee59
Shirley-Garrido97 <garridonavarroshirley@gmail.com>
2020-02-18 12:57:22 -0500

    proyecto testeado y todo ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,91 @@
 import validator from './validator.js';
 
-console.log(validator);
+// Declarando variable para llamar al boton de Siguiente y que este vaya a la 2da vista
+
+const nextPage = document.getElementById("btnSiguiente");
+nextPage.addEventListener("click", () => {
+
+document.getElementById('vista1').classList.add("ocultar");
+document.getElementById('vista2').classList.remove("ocultar");
+
+});
+
+// Declarando variable para llamar al boton de Validar y si es true la validacion que vaya a la vista 3 de lo contrario siga en vista 2
+
+const check = document.getElementById("btnValidarCard");
+check.addEventListener( "click" , () => {
+  let numCard = document.getElementById('numCard');
+  let x = validator.isvalid(numCard.value);
+  if (x == true) {
+    alert(" Tarjeta Verificada  ✅ ");
+    document.getElementById('vista2').classList.add("ocultar");
+    document.getElementById('vista3').classList.remove("ocultar");
+  } else {
+    alert(" ❌ Tarjeta NO Verificada ⚠️ ");
+  }
+
+});
+
+// Declarando variable para llamar al boton de Regresar de la vista 2 , para que me lleve a la vista 1
+
+const returnPage = document.getElementById("btnRegresar");
+returnPage.addEventListener("click", () => {
+
+document.getElementById('vista2').classList.add("ocultar");
+document.getElementById('vista1').classList.remove("ocultar");
+
+});
+
+// Declarando variable para llamar al boton de Pagar de la vista 3 , para que me lleve a la vista 4
+
+const pagar = document.getElementById("btnPagar");
+pagar.addEventListener("click", () => {
+
+document.getElementById('vista3').classList.add("ocultar");
+document.getElementById('vista4').classList.remove("ocultar");
+
+});
+
+// Declarando variable para llamar al boton de Regresar2  de la vista 3 , para que me lleve a la vista 2
+
+const returnValidatorPage = document.getElementById("btnRegresar2");
+returnValidatorPage.addEventListener("click", () => {
+
+document.getElementById('vista3').classList.add("ocultar");
+document.getElementById('vista2').classList.remove("ocultar");
+
+});
+
+// Declarando variable para llamar al boton de Finalizar de la vista 4 , para que me lleve a la vista 1
+
+const finalizarSesion = document.getElementById("btnFinalizar");
+finalizarSesion.addEventListener("click",() => {
+
+document.getElementById('vista4').classList.add("ocultar");
+document.getElementById('vista1').classList.remove("ocultar");
+
+});
+
+
+
+
+/* Declarando variable para llamar al boton de validar tarjeta.
+
+const y = document.getElementById("btnValidarCard");
+
+y.addEventListener( "click" , () =>{
+  let numCard = document.getElementById('numCard');
+  let x = validator.isvalid(numCard.value);
+  console.log(x);
+  if (x == true) {
+
+    alert(" Tarjeta Verificada  ✅ ");
+    document.getElementById('vista1').classList.add("ocultar");
+    document.getElementById('vista2').classList.remove("ocultar");
+  } else {
+    alert(" ❌ Tarjeta NO Verificada ⚠️ ");
+  }
+
+}); () => {
+
+*/
--- src/validator.js
+++ src/validator.js
@@ -1,201 +1,95 @@
-// Declarando variable para llamar al boton de Siguiente y que este vaya a la 2da vista
-
-const nextPage = document.getElementById("btnSiguiente");
-nextPage.addEventListener("click", function () {
-
-document.getElementById('vista1').classList.add("ocultar");
-document.getElementById('vista2').classList.remove("ocultar");
-
-});
-
-const returnPage = document.getElementById("btnRegresar");
-returnPage.addEventListener("click", function () {
-
-document.getElementById('vista2').classList.add("ocultar");
-document.getElementById('vista1').classList.remove("ocultar");
-
-});
-
-const pagar = document.getElementById("btnPagar");
-pagar.addEventListener("click", function () {
-
-document.getElementById('vista3').classList.add("ocultar");
-document.getElementById('vista4').classList.remove("ocultar");
-
-});
-
-const returnValidatorPage = document.getElementById("btnRegresar2");
-returnValidatorPage.addEventListener("click", function () {
-
-document.getElementById('vista3').classList.add("ocultar");
-document.getElementById('vista2').classList.remove("ocultar");
-
-});
-
-const finalizarSesion = document.getElementById("btnFinalizar");
-finalizarSesion.addEventListener("click", function () {
-
-document.getElementById('vista4').classList.add("ocultar");
-document.getElementById('vista1').classList.remove("ocultar");
-
-});
-
-// Declarando variable para llamar al boton de validar tarjeta.
-
-const validator = document.getElementById("btnValidarCard");
-
-// Se le indica que cuando se haga click que es lo que debe realizar:
-
-validator.addEventListener( "click" , function () {
-  let numCard = document.getElementById('numCard').value;
-  if (numCard.length>12 && numCard.length<=16){
-      alert("Acceso Correcto");
-    } else if (numCard.length<13){
-      alert("Falta Digitos");
-    } else {
-      alert("Exceso de Digitos")
-    }
-    console.log(parseInt(numCard)); // mostrar en consola el numero de tarjeta ingresada
-
-// Declarando una varible, que contendrá los numeros ingresados de la tarjeta estean en orden inverso
+const validator = {
+  isvalid : (numCard)=> {
 
   let reverseNumber = numCard.split("").reverse();
   console.log(reverseNumber); // mostrar en consola, los numeros ingresados en orden inverso
-
 // Separando los numeros de las posiciones Pares e Inpares.
-
   const parNumber = [];
   const inparNumber = [];
-
-  for (var i = 0; i < reverseNumber.length; i=i+1)
+  for (var i = 0; i < reverseNumber.length; i=i+1){
     if (i%2==1) {
       parNumber.push(parseInt(reverseNumber[i]*2)); // multiplicando por 2 a los numeros de las posiciones Inpares -> Originalmente son Pares
-    } else {
+    }else {
       inparNumber.push(parseInt(reverseNumber[i])); // extrayendo los numeros pares -> Originalmente son Inpares
     }
-
-// Extrayendo los numeros del arreglo ParNumber, que sean >= 10.
+  }
 
   const parNumberTwoDigit = [];
-
   for (var i = 0; i < parNumber.length; i++) {
     if(parNumber[i]>=10){
       parNumberTwoDigit.push(parNumber[i]);
     }
   }
 
-// Extrayendo los numeros del arreglo ParNumber, que sean <= 9.
-
   const parNumberOneDigit = [];
-
   for (var i = 0; i < parNumber.length; i++) {
     if(parNumber[i]<=9){
       parNumberOneDigit.push(parNumber[i]);
     }
   }
 
-
-// Sumando los numeros del arreglo ParOneDigit
-
   const sumatoriaParNumber=[];
-
   let contador1 = 0;
   for (let i = 0; i < parNumberOneDigit.length; i++) {
-    contador1 += parNumberOneDigit[i]
+    contador1 += parNumberOneDigit[i];
   }
 
   sumatoriaParNumber.push(contador1);
 
-
-// Sumando los numeros del arreglo InparNumber
-
   const sumatoriaInparNumber=[];
-
   let contador2 = 0;
   for (let i = 0; i < inparNumber.length; i++) {
-    contador2 += inparNumber[i]
+    contador2 += inparNumber[i];
   }
-
   sumatoriaInparNumber.push(contador2);
 
-// Separando el arreglo que contenia los numeros Pares >=10 y convirtiendolo en numeros de de 1 digito
-
-  var changeOneDigit = parNumberTwoDigit.join("").split("");
-
-// Conviertiendo el nuevo arreglo de parNumberTwoDigit de strings a  en Numbers
-
+  let changeOneDigit = parNumberTwoDigit.join("").split("");
   const changeArrayNumber = [];
-
-  for (var i = 0; i < changeOneDigit.length; i=i+1)
+  for (var i = 0; i < changeOneDigit.length; i=i+1){
     if (i<=16) {
       changeArrayNumber.push(parseInt(changeOneDigit[i]));
     }
-// Sumando los numeros del arreglo separado de parNumberTwoDigit
+  }
 
   const sumatoriaParTwoDigit=[];
-
   let contador3 = 0;
   for (let i = 0; i < changeArrayNumber.length; i++) {
-    contador3 += changeArrayNumber[i]
+    contador3 += changeArrayNumber[i];
   }
 
   sumatoriaParTwoDigit.push(contador3);
-
-
-// Concatenando los arreglos en 1 sólo
 
  const array1 = sumatoriaParNumber;
  const array2 = sumatoriaInparNumber;
  const array3 = sumatoriaParTwoDigit;
  const array4 = array1.concat(array2,array3);
 
-
-
-// Sumando los elementos del arreglo concatenado
-
  const sumatoriaTotal=[];
 
  let contador4 = 0;
  for (let i = 0; i < array4.length; i++) {
-   contador4 += array4[i]
+   contador4 += array4[i];
  }
 
  sumatoriaTotal.push(contador4);
 
-// Condicionando aquellos multiplos de 10 , que tipo de mensaje deberia mostrarse
-
-   for (var i = 0; i < sumatoriaTotal.length; i++)
+ for (let i = 0; i < sumatoriaTotal.length; i++){
      if(sumatoriaTotal[i]%10 == 0){
-       alert(" Tarjeta Verificada  ✅ ");
-
-       document.getElementById('vista2').classList.add("ocultar");
-       document.getElementById('vista3').classList.remove("ocultar");
-
+       return true;
      } else {
-       alert(" ❌ Tarjeta NO Verificada ⚠️ ");
-
+       return false;
      }
+ }
+},
 
-
-
-
-// Mostrando en consola todos los arreglos
-
-  console.log(parNumber);
-  console.log(parNumberTwoDigit);
-  console.log(parNumberOneDigit);
-  console.log(inparNumber);
-  console.log(sumatoriaInparNumber);
-  console.log(changeArrayNumber);
-  console.log(sumatoriaParNumber);
-  console.log(sumatoriaParTwoDigit);
-  console.log(sumatoriaTotal);
-
-
-
-
-
-} );
-
+maskify:(numCard) => {
+  let d = numCard.split("");
+  for (let i = 0; i < d.length -4; i++) {
+    d[i] = '🐱';
+  }
+    return d.join("");
+  }
+  
+};
 
 export default validator;
